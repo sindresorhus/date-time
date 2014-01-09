8da995071092d93865c9274d202fb8845f1535f4
Michael <mikach27@gmail.com>
2014-01-09 17:55:49 +0100

    Ability to print any date
    closes #1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 'use strict';
-module.exports = function () {
-	return (new Date()).toISOString().replace(/T/, ' ').replace(/\..+/, ' UTC');
+module.exports = function (date) {
+	return (date || new Date()).toISOString().replace(/T/, ' ').replace(/\..+/, ' UTC');
 };
