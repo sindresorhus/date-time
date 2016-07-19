5c47a7daa36e6b94a0e6634633da6675ee5021b9
tHBp <ramniksinghsodhi@gmail.com>
2016-07-19 20:32:17 +0530

    Added local date option for the package

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
-module.exports = function (date) {
-	return (date || new Date()).toISOString().replace(/T/, ' ').replace(/\..+/, ' UTC');
+module.exports = function(date, local) {
+	local = local ? local : date ? date instanceof Date ? null : date : null;
+	date = date ? date instanceof Date ? date : new Date() : new Date();
+	var prettyTimeZone = ' UTC';
+	if (local) {
+		prettyTimeZone = (new Date().toString().match(/GMT\S*/).toString().slice(0, 6) + ":" + new Date().toString().match(/GMT\S*/).toString().slice(6)).replace(/GMT/, "");
+		prettyTimeZone = prettyTimeZone.charAt(1) !== "0" ? prettyTimeZone : prettyTimeZone.slice(0, 1) + prettyTimeZone.slice(2);
+		prettyTimeZone = prettyTimeZone.split(":")[1] === "00" ? prettyTimeZone.split(":")[0] : prettyTimeZone;
+		prettyTimeZone = ' UTC'+ prettyTimeZone;
+	}
+	return date.toISOString().replace(/T/, ' ').replace(/\..+/, prettyTimeZone);
 };
--- test.js
+++ test.js
@@ -2,6 +2,22 @@
 var assert = require('assert');
 var dateTime = require('./');
 
-it('should print out pretty datetime', function () {
+it('should print out UTC datetime', function () {
 	assert(/UTC$/.test(dateTime()));
+	console.log(dateTime());
+});
+
+it('should print out local datetime', function(){
+	assert(/(UTC[\+\-][1-9][0-9]?(:30)?$)|(UTC[\+\-]0$)/.test(dateTime('local')));
+	console.log(dateTime(true));
+});
+
+it('should print out UTC datetime of passed date', function(){
+	assert(/UTC$/.test(dateTime(new Date(2050, 1, 2))));
+	console.log(dateTime(new Date(2050, 1, 2)));
+});
+
+it('should print out local datetime of passed date', function(){
+	assert(/(UTC[\+\-][1-9][0-9]?(:30)?$)|(UTC[\+\-]0$)/.test(dateTime(new Date(2050, 1, 2), 'local')));
+	console.log(dateTime(new Date(2050, 1, 2), 'local'));
 });
