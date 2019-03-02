07209b77554982c58569797bcce9084a3f72c373
Sindre Sorhus <sindresorhus@gmail.com>
2019-03-03 00:13:15 +0700

    Update index.test-d.ts

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,14 +1,9 @@
 import {expectType} from 'tsd-check';
-import DateTime from ".";
+import dateTime from '.';
 
-expectType<string>(DateTime());
-
-expectType<string>(DateTime({date: new Date(1989, 2, 4, 10)}));
-
-expectType<string>(DateTime({showTimeZone: true}));
-
-expectType<string>(DateTime({local: false}));
-
-expectType<string>(DateTime({local: false, showTimeZone: true}));
-
-expectType<string>(DateTime({showMilliseconds: true}));
+expectType<string>(dateTime());
+expectType<string>(dateTime({date: new Date(1989, 2, 4, 10)}));
+expectType<string>(dateTime({showTimeZone: true}));
+expectType<string>(dateTime({local: false}));
+expectType<string>(dateTime({local: false, showTimeZone: true}));
+expectType<string>(dateTime({showMilliseconds: true}));
